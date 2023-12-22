e36283597f71e7d8b6c091bf95fe3a67b8a9e488
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2023-12-22 19:03:24 +0530

    fix

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # main.py
 from app import create_app, socketio
 
-if __name__ == '__main__':
-    app = create_app()
-    socketio.init_app(app)  # Initialize socketio with the app
-    socketio.run(app, debug=True)
+app = create_app()
+socketio.init_app(app)  # Initialize socketio with the app
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
