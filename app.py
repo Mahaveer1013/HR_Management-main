29b67a5f3fe63a5ea2ddba6b3f0ee4980d60dd69
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2023-12-23 07:12:28 +0530

    fix

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # main.py
 from app import create_app, socketio
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
 socketio.init_app(app)  # Initialize socketio with the app
 
-# if __name__ == '__main__':
-#     socketio.run(app, debug=True)
+# Adding WSGI middleware (ProxyFix for Nginx)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
